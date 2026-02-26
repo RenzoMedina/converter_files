d663da01736b6a61ed6ae432a39bf33dbdf913ff
RenzoMedina <medinast30@gmail.com>
2026-02-25 23:30:31 -0300

    Modified regex and adding page 404 and 500

---
+++ public/500.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="public/assets/img/icon.ico" type="image/x-icon">
+    <title>500 Internal Server Error</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
+    <link rel="stylesheet" href="public/style.css">
+</head>
+<body>
+    Error 500: Internal Server Error. Something went wrong on our end. Please try again later.
+</body>
+</html>
