df9a5ffb67b5eafe2658aab2bec3b418714dd385
RenzoMedina <medinast30@gmail.com>
2026-06-04 21:33:21 -0400

    Fix: fixed bug correction word respuesta correcta into options

--- app/Utils/RegexUtils.php
+++ app/Utils/RegexUtils.php
@@ -9,7 +9,7 @@ class RegexUtils
     public const PROCCESS_IMPORT_BLOCK = '/N°\s*de\s*pregunta:?\s*(\d+)\s+(.*?)(?:\s*Retroalimentación:?\s*(.*?))?$/s';
     public const QUESTION = '/N°\s*de\s+pregunta:?\s*(\d+)\s*(.*?)\s*Retroalimentación:?\s*(.*?)(?=N°\s*de\s+pregunta:?|$)/s';
     public const EXTRACT_INDICATORS = '/Indicador\s+(\d+):\s*([^\n]*)/s';
-    public const EXTRACT_OPTIONS_MULTICHOICES = '/Alternativas\s*(.*?)(?=Indicador\s+de\s+evaluación|Respuesta\s+correcta)/s';
+    public const EXTRACT_OPTIONS_MULTICHOICES = '/Alternativas\s*(.*?)(?=Indicador\s+de\s+evaluación|^Respuesta\s+correcta)/ms';
     public const OPTIONS_MULTICHOICES = '/([a-e])\)\s*(.*?)(?=\s*[a-e]\)|$)/s';
     public const EXTRACT_ESSAY = '/Escribe aquí tu respuesta/s';
     public const EXTRACT_OPTION_TRUE_FALSE = '/Verdadero\s+o\s+falso\s*(.*?)(?=Respuesta\s+correcta)/si';
@@ -19,5 +19,5 @@ class RegexUtils
     public const CLEAN_ALTERNATIVES = '/\s+Alternativas\s+[a-e]\).*$/is';
     public const CLEAN_ESSAY = '/\s*Escribe\s+aquí\s+tu\s+respuesta.*$/si';
     public const CLEAN_TRUE_FALSE = '/\s+Verdadero\s+o\s+falso\s+[a-b]\).*$/is';
-    public const CLEAN_ANSWER = '/Respuesta\s*correcta\s*([a-e]|verdadero|falso)/si';
+    public const CLEAN_ANSWER = '/^Respuesta\s*correcta\s*([a-e]|verdadero|falso)\s*$/mi';
 }
